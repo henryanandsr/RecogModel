374f6926bd504a2a264471911b0cd12a86d0db71
henryanandsr <13521004@std.stei.itb.ac.id>
2023-09-04 03:44:33 +0700

    fix : model

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from google.cloud import storage
 import tensorflow as tf
 from PIL import Image
 import numpy as np
+import cv2
 
 app = Flask(__name__)
 def load_model_from_gcs(bucket_name, blob_name):
@@ -24,40 +25,27 @@ def predict():
     try:
         # Get the image file from the request
         file = request.files['file']
-        img = Image.open(file).convert('RGB')
-        img = img.resize((224, 224))
-        
-        # Preprocess the image
+        image = Image.open(file).convert('RGB')
+        image = cv2.resize(np.array(image), (224, 224))
+
+        # Normalize the image
+        image = (image / 255.0).astype(np.float32)
+
+        # Make predictions
         input_details = model.get_input_details()
         output_details = model.get_output_details()
-        
-        print("Input details length:", len(input_details))
-        print("Output details length:", len(output_details))
-        print("Input details:", input_details)
-        print("Output details:", output_details)
-        
-        if len(input_details) > 0 and len(output_details) > 0:
-            quantization = input_details[0]['quantization']
-            input_scale = quantization[0] if len(quantization) > 0 and quantization[0] != 0 else 1.0
-            input_mean = quantization[1]
-            input_std = quantization[2]
-            input_data = np.array(img, dtype=np.float32) / input_scale
-            input_data = (input_data - input_mean) / input_std
-            input_data = np.expand_dims(input_data, axis=0)
-            
-            # Make predictions
-            model.set_tensor(input_details[0]['index'], input_data)
-            model.invoke()
-            output_data = model.get_tensor(output_details[0]['index'])
-            predicted_class = np.argmax(output_data)
-            confidence = output_data[0][predicted_class]
-            
-            response = {
-                "predicted_class": int(predicted_class),
-                "confidence": float(confidence)
-            }
-            return jsonify(response)
-        
+        model.set_tensor(input_details[0]['index'], image.reshape(1, 224, 224, 3))
+        model.invoke()
+        prediction = model.get_tensor(output_details[0]['index'])
+        class_index = np.argmax(prediction, axis=1)[0]
+        confidence = prediction[0][class_index]
+
+        response = {
+            "predicted_class": int(class_index),
+            "confidence": float(confidence)
+        }
+        return jsonify(response)
+
     except Exception as e:
         print("Error:", e)
         return jsonify({"error": str(e)})
